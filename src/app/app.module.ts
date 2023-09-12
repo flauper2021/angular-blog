2ab0959132c3611289132f4c4a1b74c7e097bb5c
mauricioJuica <mauricio.work@yahoo.com>
2023-09-12 09:27:14 -0300

    components created with styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { TitleBarComponent } from './components/title-bar/title-bar.component';
+import { LargeCardComponent } from './components/large-card/large-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    TitleBarComponent,
+    LargeCardComponent,
+    SmallCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
