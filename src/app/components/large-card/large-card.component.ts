e79e45e964fd1bc9841e3efc7f537fa5c5b987d2
mauricioJuica <mauricio.work@yahoo.com>
2023-09-12 10:58:46 -0300

    components dinamycs propertys

--- src/app/components/large-card/large-card.component.ts
+++ src/app/components/large-card/large-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-large-card',
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./large-card.component.css']
 })
 export class LargeCardComponent implements OnInit {
+
+  @Input()
+  photoCard: string = ""
+  @Input()
+  cardTitle: string = ""
+  @Input()
+  cardDescription: string = ""
 
   constructor() { }
 
